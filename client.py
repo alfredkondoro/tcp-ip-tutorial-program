6737888c64993d7a342289799858557b1660c472
Alfred Kondoro <alfredkondoro97@gmail.com>
2020-08-20 17:01:56 +0300

    version 2.0

--- client.py
+++ client.py
@@ -1,15 +1,21 @@
 import socket
 
+
 c = socket.socket()
 
 c.connect(('localhost', 9999))
 
-print(c.recv(1024).decode())
-
 if True:
-    name = input('Enter your name')
-    c.send(bytes(name, 'utf-8'))
-
     print(c.recv(1024).decode())
-else:
-    print("Sorry, there is no connection at the moment")
+
+    while True:
+        data = input('Input: ')
+        if data == 'Close':
+            ins = 'yes'
+            c.send(bytes(ins, 'utf-8'))
+            break
+        else:
+            c.send(bytes(data, 'utf-8'))
+            print(c.recv(1024).decode())
+
+c.close()
--- server.py
+++ server.py
@@ -1,25 +1,33 @@
 import socket
 
+# create the socket
 s = socket.socket()
 print('Socket Created')
 
 s.bind(('localhost', 9999))
 
 s.listen(3)
-print('Waiting for connections')
+print('Ready waiting for connections')
 
 while True:
+
     c, addr = s.accept()
 
     print("Connected with", addr)
     c.send(bytes('Welcome to Alfreds Server', 'utf-8'))
 
-    name = c.recv(1024).decode()
-    print("Our client\'s name is", name)
-
-    if True:
-        c.send(bytes('Thank you for working with us', 'utf-8'))
-
-    else:
-        print("We could not connect with our clients at the moment")
-    c.close()
+    while True:
+        ins = c.recv(1024).decode()
+        if ins == 'yes':
+            c.close()
+            close = input('The client has closed connection, kindly close yours(y/n)')
+            if close == 'y':
+                print('Closing...')
+                s.close()
+                print('Closed')
+                break
+        else:
+            print('Client:', ins)
+            reply = input('Server: ')
+            c.send(bytes(reply, 'utf-8'))
+            continue
